9cbc2166b2a2c765ad4d50a15e4dd36eea501c5c
fharaujo <faraujodeveloper@hotmail.com>
2021-09-28 22:35:35 -0300

    create prisma service and prisma module

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
+// conexão com o banco
 @Injectable()
-export class PrismaService {}
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  // fechar a conexão com o banco
+  async enableShutdownHooks(app: INestApplication) {
+    this.$on('beforeExit', async () => {
+      await app.close();
+    });
+  }
+}
