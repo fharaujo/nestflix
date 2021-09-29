0646e861c213b5d11d07e652e6301baaf47ddb8b
fharaujo <faraujodeveloper@hotmail.com>
2021-09-28 23:11:19 -0300

    functions gelAll and getById create

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { createMoviesDto } from './dto/create.movies.dto';
+import { Movie } from '.prisma/client';
 
-@Controller('movies')
-export class MoviesController {}
+@Controller('v1/movies')
+export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
+
+  // route get all movies
+  @Get('/list')
+  @UsePipes(ValidationPipe)
+  async getAll(): Promise<Movie[]> {
+    return this.moviesService.findMany();
+  }
+
+  // route by id movies
+  @Get('/list/:id')
+  @UsePipes(ValidationPipe)
+  async getByIdMovie(@Param('id', ParseIntPipe) id: number) {
+    return this.moviesService.getByIdMovies(id);
+  }
+}
--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
-
+import { Movie, Prisma } from '.prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 @Injectable()
-export class MoviesService {}
+export class MoviesService {
+  constructor(private prisma: PrismaService) {}
+
+  async findMany(): Promise<Movie[]> {
+    return this.prisma.movie.findMany();
+  }
+
+  async getByIdMovies(movieId: number): Promise<Movie> {
+    return this.prisma.movie.findUnique({
+      where: {
+        id: movieId,
+      },
+      include: {
+        participants: {
+          select: {
+            name: true,
+            image: true,
+            birth: true,
+            staff: true,
+          },
+        },
+        genre: {
+          select: {
+            name: true,
+          },
+        },
+      },
+    });
+  }
+}
