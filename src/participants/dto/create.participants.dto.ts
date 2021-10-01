16d9a42f2f664c113ccd07879dd0943a2773d1b8
fharaujo <faraujodeveloper@hotmail.com>
2021-10-01 19:44:22 -0300

    refactory participants dto

--- src/participants/dto/create.participants.dto.ts
+++ src/participants/dto/create.participants.dto.ts
@@ -2,7 +2,13 @@ import { IsNotEmpty } from 'class-validator';
 export class createParticipantsDto {
   @IsNotEmpty()
   name: string;
+
+  @IsNotEmpty()
   image: string;
+
+  @IsNotEmpty()
   birth: string;
+
+  @IsNotEmpty()
   staff: string;
 }
