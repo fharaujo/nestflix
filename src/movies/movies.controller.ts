24caece91a9e8a75c492701f2d96b374d4fd28e3
fharaujo <faraujodeveloper@hotmail.com>
2021-10-01 21:46:19 -0300

    create doc swagger movie

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -13,7 +13,7 @@ import {
 import { MoviesService } from './movies.service';
 import { createMoviesDto } from './dto/create.movies.dto';
 import { Movie } from '.prisma/client';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 
 @Controller('v1/movies')
 @ApiTags('Movies')
@@ -23,12 +23,14 @@ export class MoviesController {
   // route get all movies
   @Get('/list')
   @UsePipes(ValidationPipe)
+  @ApiOperation({ summary: 'List all movies' })
   async getAll(): Promise<Movie[]> {
     return this.moviesService.findMany();
   }
 
   // route by id movies
   @Get('/list/:id')
+  @ApiOperation({ summary: 'Print by id movie' })
   @UsePipes(ValidationPipe)
   async getByIdMovie(@Param('id', ParseIntPipe) id: number) {
     return this.moviesService.getByIdMovies(id);
@@ -36,6 +38,7 @@ export class MoviesController {
 
   // route create movie
   @Post('/create')
+  @ApiOperation({ summary: 'Create movie' })
   @UsePipes(ValidationPipe)
   async createMovie(@Body() data: createMoviesDto): Promise<Movie> {
     return this.moviesService.createMovie(data);
@@ -43,6 +46,7 @@ export class MoviesController {
 
   // route update movie
   @Put('/update/:id')
+  @ApiOperation({ summary: 'Update movie by id' })
   @UsePipes(ValidationPipe)
   async updateMovie(
     @Param('id', ParseIntPipe) id: number,
@@ -53,6 +57,7 @@ export class MoviesController {
 
   // route delete one movie
   @Delete('/delete/:id')
+  @ApiOperation({ summary: 'Delete movie by id' })
   @UsePipes(ValidationPipe)
   async deleteOneMovie(@Param('id', ParseIntPipe) id: number): Promise<Movie> {
     return this.moviesService.deleteMovie(id);
